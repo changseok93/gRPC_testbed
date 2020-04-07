18955fc8836cbf5053798944093dc0686a3ee134
changseok <changseok93@icloud.com>
2020-04-07 15:26:17 +0900

    minor bug change

--- my_client.py
+++ my_client.py
@@ -7,12 +7,12 @@ import my_protobuf_pb2
 import my_protobuf_pb2_grpc
 
 def run():
-    input_image = open('input_data.jpg', 'rb')
-    input_image = input_image.read()
+    input_image = open('input_data.jpg', 'rb').read()
+    
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = my_protobuf_pb2_grpc.transactionTestStub(channel)
         response = stub.IimageOtext(my_protobuf_pb2.ImageType(format='jpg', data = input_image))
-    print('{} send complete'.format(response.result))
+    print('{} send complete'.format(response.format))
 
 if __name__ == '__main__':
     run()
--- my_server.py
+++ my_server.py
@@ -7,20 +7,20 @@ import my_protobuf_pb2
 import my_protobuf_pb2_grpc
 
 
-class ImageIO(my_protobuf_pb2_grpc.transactionTestServicer):
+class transactionTest(my_protobuf_pb2_grpc.transactionTestServicer):
     def __init__(self):
         self.file_counter = 1
 
     def IimageOtext(self, img, context):
         file = open('output_data_{}.jpg'.format(self.file_counter), 'wb')
-        file.write(img.File)
+        file.write(img.data)
         file.close()
         self.file_counter+=1
         return my_protobuf_pb2.ImageType(format = img.format, data = img.data)
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    my_protobuf_pb2_grpc.add_transactionTestServicer_to_server(ImageIO(), server)
+    my_protobuf_pb2_grpc.add_transactionTestServicer_to_server(transactionTest(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
